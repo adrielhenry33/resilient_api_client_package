cae5546b32fcee9761e3cb19e3dc08cf51df6fae
Adriel Henry Dias Barbosa <adrielhenrydb@gmail.com>
2026-03-04 14:13:22 -0300

    first commit

---
+++ example/resilient_client_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:resilient_client_api/resilient_client_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/resilient_client_api_test.dart
@@ -0,0 +1,16 @@
+import 'package:resilient_client_api/resilient_client_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
