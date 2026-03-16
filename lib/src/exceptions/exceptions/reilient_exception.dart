cb157169df2b188075adc55b52e643511eff7e7c
Adriel Henry Dias Barbosa <adrielhenrydb@gmail.com>
2026-03-16 11:52:11 -0300

    feat: cria a classe base para todas as exceções

---
+++ lib/src/exceptions/exceptions/reilient_exception.dart
@@ -0,0 +1,31 @@
+/// Classe base abstrata para todas as execções do [ResilientApiClient]
+///
+/// Ao herdar de [Exception], garantimos que o erro possa ser capturado
+///em blocos try-catch espcíficos.
+
+library;
+
+abstract class ResilientException implements Exception {
+  final String message;
+
+  final int? statusCode;
+
+  ResilientException(this.message, [this.statusCode]);
+
+  @override
+  String toString() => '$runtimeType: $message (Status: $statusCode)';
+}
+
+/// Lançada quando o número máximo de tentativas (retries) é esgotado.
+class RetryLimitExceededException extends ResilientException {
+  RetryLimitExceededException(String message, [int? statusCode])
+    : super(message, statusCode);
+}
+
+/// Lançada quando o servidor retorna um erro de status (ex.: 4xx , 5xx)
+class ResilientStatusException extends ResilientException {
+  final String? responsyBody;
+
+  ResilientStatusException(String message, int statusCode, {this.responsyBody})
+    : super(message, statusCode);
+}
