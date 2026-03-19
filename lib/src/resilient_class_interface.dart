2f10c3887458b86cd399a18306d00ec62e413dde
Andriel Henry Dias Barbosa <adrielhenrydb@gmail.com>
2026-03-19 08:27:10 -0300

    refactor: troca o tipo do retorno das funcoes e adiciona uma nova

--- lib/src/resilient_class_interface.dart
+++ lib/src/resilient_class_interface.dart
@@ -1,6 +1,7 @@
-import 'models/resilient_options_model.dart';
-import 'resilient_client_api_base.dart';
+import 'dart:io';
 
+import 'models/resilient_options_model.dart';
+import 'package:http/http.dart';
 
 /// O contrato(interface) que define como a classe ira se comportar
 /// 
@@ -11,11 +12,17 @@ library;
 abstract class ResilientClassInterface {
   ResilientOptionsModel get options;
 
-  /// Realiza uma requisição GET resilitente
-  Future<dynamic> get(String url, {Map<String, dynamic> headers});
+  /// Realiza uma requisição GET resilitente bruta de baixo nivlle 
+  Future<Response> get(String url,{Map<String, dynamic> headers});
+
+
+  ///Metodo de alto nivel: ja retorna o objeto [T] convertido.
+  ///[decoder] é uma função que transforma a String jso  no seu modelo
+  
+  Future<Response> getTyped<T>(String url, {required T Function(String) decoder, Map<String,dynamic>? headers});
 
   /// realiza uma requisição POST  resiliente
-  Future<dynamic> post(String url,{Map<String, dynamic> data, Object? body} );
+  Future<Response> post(String url,{Map<String, dynamic> data, Object? body} );
 
 
 
